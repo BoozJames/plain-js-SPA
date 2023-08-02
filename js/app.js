51422b267ff7e1b668f9fdf036d3fbcdbc7a4f80
BoozJames <jamesboozmanumbale@gmail.com>
2023-08-02 12:59:34 +0800

    init

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+(function () {
+    function init() {
+        var router = new Router([
+            new Route('home', 'home.html', true),
+            new Route('about', 'about.html'),
+            new Route('contact', 'contact.html'),
+            new Route('portfolio', 'portfolio.html')
+        ]);
+    }
+
+    init();
+}());
